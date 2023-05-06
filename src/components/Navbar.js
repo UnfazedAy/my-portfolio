0e7b290f1e842cfff07d563e70ab85c9e156e24f
UnfazedAy <ayomidesoniyi@gmail.com>
2023-05-06 16:08:46 +0100

    Adding the components and making some changes

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+export default function Navbar() {
+  return (
+    // Navbar
+    <nav className="relative container mx-auto p-6">
+      {/* Flex Container */}
+      <div className="flex items-center justify-between">
+        <div className="pt-2">
+          <h1 className="font-bold font-lobster text-2xl md:text-3xl">
+            Ayomide Soniyi<span className="text-thickBrownishYellow text-4xl md:text-5xl">.</span>
+          </h1>
+        </div>
+        <div className="hidden space-x-6 md:flex">
+          <a href="#about" className="hover:text-thickBrownishYellow">
+            About
+          </a>
+          <a href="#project" className="hover:text-thickBrownishYellow">
+            Project
+          </a>
+          <a href="#resume" className="hover:text-thickBrownishYellow">
+            Resume
+          </a>
+          <a
+            href="#contact"
+            className="inline-block text-white bg-thickBrownishYellow rounded-full baseline hover:bg-brownishYellow focus:outline-none px-5 drop-shadow-md"
+          >
+            Contact
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        fadedPink: 'hsl(13, 100% 96%)',
+        lightGray: 'hsl(240, 5%, 92%)',
+        brownishYellow: 'hsl(37, 97%, 73%)',
+        thickBrownishYellow: 'hsl(39, 97%, 55%)',
+        cloudBlue: 'hsl(190, 89%, 85%)',
+        lightBlue: 'hsl(188, 85%, 95%)',
+      }
+    },
+    fontFamily: {
+      'lobster': ['Lobster', 'cursive']
+    },
+  },
+  plugins: [],
+}
+
