9bae5a8336e39c623405942a256e6db10704f2e7
UnfazedAy <ayomidesoniyi@gmail.com>
2023-06-16 12:21:28 +0100

    Fixed the error with the directory path to the Resume link

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,7 +3,7 @@ export default function Resume() {
     <div id="resume" className="container mx-auto p-6">
       <div className="flex justify-center md:justify-start">
         <button className="bg-thickBrownishYellow md:w-1/3 text-black mt-5 font-semibold py-3 px-3 resume-download rounded-full drop-shadow-md hover:bg-brownishYellow focus:outline-none">
-          <a href="/public/Ayomide_Soniyi_Resume_16-06-2023-11-55-40.pdf" download="Ayomide-Soniyi.pdf">
+          <a href="/Ayomide_Soniyi_flow.pdf" download="Ayomide-Soniyi.pdf">
             Download My Resume
           </a>
         </button>
